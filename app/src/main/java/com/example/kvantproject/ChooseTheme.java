54ee7beda9654545ca51eb2a1cb3c97cb5f9ef42
SpaceScout <nelindanila7@gmail.com>
2021-12-15 02:16:00 +0300

    settings2

--- app/src/main/java/com/example/kvantproject/ChooseTheme.java
+++ app/src/main/java/com/example/kvantproject/ChooseTheme.java
@@ -1,4 +1,22 @@
 package com.example.kvantproject;
 
-public class ChooseTheme {
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChooseTheme extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        int theme = sp.getInt("THEME", R.);
+        setTheme(theme);
+    }
+
+
 }
