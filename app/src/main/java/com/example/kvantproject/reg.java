41e80e1937c6792fe714fa0a41134f3052059010
SpaceScout <nelindanila7@gmail.com>
2022-03-10 00:55:20 +0300

    reg_1_time

--- app/src/main/java/com/example/kvantproject/reg.java
+++ app/src/main/java/com/example/kvantproject/reg.java
@@ -1,52 +1,95 @@
 package com.example.kvantproject;
 
+import android.content.ContentValues;
 import android.content.Intent;
-import android.graphics.Color;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+//import com.example.kvantproject.MainActivity;
+import com.example.kvantproject.R;
+
+
 public class reg extends AppCompatActivity implements View.OnClickListener {
+    EditText etName, etPassword;
 
-    private EditText username;
-    private EditText password;
-    private Button login;
+    Intent intent;
 
-    //число для подсчета попыток
-    int numberOfRemainingLoginAttempts = 10;
+    public static final String PREFS_NAME = "MyPrefsLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reg);
 
-        username = findViewById(R.id.edit_user);
-        password = findViewById(R.id.edit_password);
-        Button login = findViewById(R.id.button_login);
+        Button btnAdd = findViewById(R.id.registr);
+        btnAdd.setOnClickListener(this);
 
-        login.setOnClickListener(this);
+        Button btnRead = findViewById(R.id.button_login);
+        btnRead.setOnClickListener(this);
+
+        etName = (EditText) findViewById(R.id.edit_user);
+        etPassword = (EditText) findViewById(R.id.edit_password);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        intent = new Intent(reg.this, MainActivity.class);
+        Log.i("Mlog","ща резюм работает");
+
+
+        SharedPreferences settings = getSharedPreferences(reg.PREFS_NAME, 0);
+        //Get "hasLoggedIn" value. If the value doesn't exist yet false is returned
+
+        if(settings.getBoolean("hasLoggedIn", false))
+        {
+            setContentView(R.layout.activity_main);
+            Log.i("Mlog","корорче сработало по резюму");
+        }
+
     }
     @Override
-    public void onClick(View v){
-        // Если введенные логин и пароль будут словом "admin",
-        // показываем Toast сообщение об успешном входе:
-        if (username.getText().toString().equals("admin") &&
-                password.getText().toString().equals("admin")) {
-            Toast.makeText(getApplicationContext(), "Вы успешно вошли", Toast.LENGTH_SHORT).show();
-            // Выполняем переход на другой экран:
-            Intent intent = new Intent(reg.this, MainActivity.class);
-            startActivity(intent);
-        }
-        // В другом случае выдаем сообщение с ошибкой
-        else {
-            Toast.makeText(getApplicationContext(), "Неправильные данные", Toast.LENGTH_SHORT).show();
-            numberOfRemainingLoginAttempts--;
+    public void onClick(View view) {
+        String name = etName.getText().toString();
+        String password = etPassword.getText().toString();
 
-            }
+
+        ContentValues contentValues = new ContentValues();
+
+        int id = view.getId();
+        if (id == R.id.registr) {
+
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "вы успешно зарегались", Toast.LENGTH_SHORT);
+            toast.show();
+        } else if (id == R.id.button_login) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "вы успешно вошли", Toast.LENGTH_SHORT);
+            toast.show();
+            setContentView(R.layout.activity_main);
+
+            //User has successfully logged in, save this information
+            // We need an Editor object to make preference changes.
+            SharedPreferences settings = getSharedPreferences(reg.PREFS_NAME, 0); // 0 - for private mode
+            SharedPreferences.Editor editor = settings.edit();
+
+            //Set "hasLoggedIn" to true
+            editor.putBoolean("hasLoggedIn", true);
+
+            // Commit the edits!
+            editor.commit();
+
+            Log.i("Mlog","корорче теперь фолс по идеи");
+        }
     }
 }
